b3a308de2a4dd97449a3ffa88aa699430253b9f6
saynschuman <saynschuman@gmail.com>
2020-08-24 09:31:52 +0300

    build

---
+++ build/precache-manifest.62fbb8cb39fa66ed0556cb5142577eb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9e26272eb671321cea040322626255",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e7abba023203bce810",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "26ae65e60a1898263b77",
+    "url": "/static/css/main.96cb135a.chunk.css"
+  },
+  {
+    "revision": "45e7abba023203bce810",
+    "url": "/static/js/2.281f9abc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.281f9abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ae65e60a1898263b77",
+    "url": "/static/js/main.9551b7c7.chunk.js"
+  },
+  {
+    "revision": "375adf367b25e07a5594",
+    "url": "/static/js/runtime-main.ee291752.js"
+  }
+]);
