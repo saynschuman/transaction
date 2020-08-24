d02787e601d22ed923935b9b4c0906120f65b0ef
saynschuman <saynschuman@gmail.com>
2020-08-24 08:57:56 +0300

    fields render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Text, Box, Input, Button, Container, Select } from '@chakra-ui/core'
+import './App.css'
 
 function App() {
+  const [values, setValues] = React.useState({
+    file: {},
+    from: '',
+    to: '',
+    code: '',
+    select: '',
+  })
+  const onChange = (e, type) => {
+    if (type === 'file') {
+      setValues({
+        ...values,
+        file: e.target.files[0],
+      })
+    } else {
+      if (type === 'to' || type === 'from') {
+        setValues({
+          ...values,
+          [type]: Math.round(new Date(e.target.value).getTime() / 1000),
+        })
+      } else {
+        setValues({
+          ...values,
+          [type]: e.target.value,
+        })
+      }
+    }
+  }
+
+  const submit = () => {
+    console.log(values)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Box display="flex" mb="10px" alignItems="center">
+        <Input type="file" accept=".xml,.csv" onChange={(e) => onChange(e, 'file')} />
+        <Button ml="10px">import</Button>
+      </Box>
+      <Box mb="10px">
+        <Input type="text" placehilder="Код валюты" onChange={(e) => onChange(e, 'code')} />
+      </Box>
+      <Box mb="10px">
+        <Select onChange={(e) => onChange(e, 'select')}>
+          <option value="1">Approved</option>
+          <option value="2">Failed</option>
+          <option value="3">Rejected</option>
+          <option value="4">Finished</option>
+          <option value="5">Done</option>
+        </Select>
+      </Box>
+      <Box mb="10px" display="flex">
+        <Text mr="5px" fontWeight="bold">
+          from
+        </Text>
+        <Input type="date" onChange={(e) => onChange(e, 'from')} />
+      </Box>
+      <Box mb="10px" display="flex">
+        <Text mr="5px" fontWeight="bold">
+          to
+        </Text>
+        <Input type="date" onChange={(e) => onChange(e, 'to')} />
+      </Box>
+      <Box>
+        <Button onClick={submit}>Get</Button>
+      </Box>
+    </Container>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { ChakraProvider, CSSReset } from '@chakra-ui/core'
+import theme from '@chakra-ui/theme'
+
+const customTheme = {
+  ...theme,
+  breakpoints: ['768px', '991px', '1440px', '1920px'],
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
